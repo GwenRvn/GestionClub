8386e7941593a14d38b5ca749f65b8b15313bd02
Gwendoline Raveino <naheirvn@gmail.com>
2024-04-16 13:40:57 -1000

    Initial commit

---
+++ GestionClub/resources/views/pages/apropos.blade.php
@@ -0,0 +1,23 @@
+@extends("layouts.master")
+@section('title')
+    À propos
+@endsection
+@section('content')
+    <div class="container " style="margin-top: 50px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; padding: 40px; border-radius: 20PX;">
+
+        <h1 class="text-center">À propos</h1>
+        <h2>Chers visiteurs</h2>
+
+        <p>Personnels, Parents, Elèves, Sympathisants,</p>
+
+        <p>C'est avec plaisir que je vous accueille sur notre site internet au nom de l'ensemble des personnels qui y travaillent. Je vous souhaite une expérience visiteur agréable et surtout qui vous permette de trouver l'information que vous recherchez. N'hésitez pas à prendre contact si vous estimez ne pas obtenir la précision de l'information recherchée.</p>
+
+        <p>Le lycée du Diademe est un lycée polyvalent qui déploie une carte des formations très riche et un panel d'activités pédagogiques qui répond au plus grand nombre. Les personnels qui y travaillent sont des professionnels aguerris et engagés pour la réussite scolaire. Nous mettons tout en oeuvre pour accompagner tous les élèves qui nous sont confiés vers la réussite de leur parcours qu'ils soient inscrits dans le pôle industriel, tertiaire ou général. Ainsi, c'est dans un esprit de coéducation qui lie valeurs républicaines (laïcité, école inclusive, égalité homme-femme/fille-garçon) et multiculturalité que nous souhaitons poursuivre nos actions au travers d'un projet d'établissement riche et fourni. Notre pédagogie vise à former des jeunes gens ouverts, de futurs citoyens éclairés et conscients des grands enjeux planétaires.</p>
+
+        <p>Nous nous engageons à oeuvrer dans un climat scolaire rasséréné qui valorise le vivre ensemble et la bienveillance, l'assiduité, et la persévérance scolaire.</p>
+
+        <p>Je vous souhaite une belle expérience navigateur, et espère avoir le plaisir de vous retrouver très prochainement dans nos locaux.</p>
+
+        <h2>Pascal CHARLERY Proviseur</h2>
+    </div>
+@endsection
